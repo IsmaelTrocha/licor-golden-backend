fda8a3ce38c56c438b4502df3e0b469ab9722e55
Quintero45 <millerquintero1@gmail.com>
2023-10-24 03:10:53 -0500

    Salvacion

---
+++ src/main/java/com/liquorsgolden/lq/domain/services/proportion/GetAllProportionService.java
@@ -0,0 +1,11 @@
+package com.liquorsgolden.lq.domain.services.proportion;
+
+import com.liquorsgolden.lq.domain.entities.Proportion;
+import com.liquorsgolden.lq.domain.entities.Status;
+
+import java.util.List;
+
+public interface GetAllProportionService {
+
+    List<Proportion> getAllProportion();
+}
