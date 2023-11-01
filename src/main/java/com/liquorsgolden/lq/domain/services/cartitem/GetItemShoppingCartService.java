2e24087be55a0dd4bbe3d804f39488ed2c3c1384
Ismael Trocha <ismael.trocha@hotmail.com>
2023-11-01 18:45:37 -0500

    No-add

--- src/main/java/com/liquorsgolden/lq/domain/services/cartitem/GetItemShoppingCartService.java
+++
@@ -1,9 +0,0 @@
-package com.liquorsgolden.lq.domain.services.cartitem;
-
-import com.liquorsgolden.lq.domain.entities.CartItem;
-import java.util.List;
-
-public interface GetItemShoppingCartService {
-
-  List<CartItem> allItems();
-}
--- src/main/java/com/liquorsgolden/lq/domain/services/cartitem/UpdateQuantityItemCartService.java
+++
@@ -1,6 +0,0 @@
-package com.liquorsgolden.lq.domain.services.cartitem;
-
-public interface UpdateQuantityItemCartService {
-
-  void updateQuantity(int quantity, Long id);
-}
