06d152ff0cc1fec9d34b5d0562e6bbe97fe80580
Quintero45 <millerquintero1@gmail.com>
2023-11-01 15:12:09 -0500

    venta (no terminada)

---
+++ src/main/java/com/liquorsgolden/lq/domain/entities/ClasePrueba.java
@@ -0,0 +1,11 @@
+package com.liquorsgolden.lq.domain.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ClasePrueba {
+}
