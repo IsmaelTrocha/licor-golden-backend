17b73f7f21d5f223708228ac461af9a33c91bfac
Ismael Trocha <ismael.trocha@hotmail.com>
2023-11-01 23:40:10 -0500

    Adding Cart entity

--- src/main/java/com/liquorsgolden/lq/application/order/CreateOrderApplication.java
+++ src/main/java/com/liquorsgolden/lq/application/order/CreateOrderApplication.java
@@ -2,7 +2,6 @@ package com.liquorsgolden.lq.application.order;
 
 import com.liquorsgolden.lq.domain.entities.Order;
 import com.liquorsgolden.lq.domain.services.order.CreateOrderService;
-import com.liquorsgolden.lq.infrastructure.api.dto.request.order.OrderRequest;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
--- src/main/java/com/liquorsgolden/lq/infrastructure/adapter/order/CreateOrderAdapter.java
+++ src/main/java/com/liquorsgolden/lq/infrastructure/adapter/order/CreateOrderAdapter.java
@@ -1,17 +1,13 @@
 package com.liquorsgolden.lq.infrastructure.adapter.order;
 
-import com.liquorsgolden.lq.domain.entities.Product;
+import com.liquorsgolden.lq.domain.entities.Order;
 import com.liquorsgolden.lq.domain.services.order.CreateOrderService;
 import com.liquorsgolden.lq.domain.services.product.GetProductByIdService;
 import com.liquorsgolden.lq.domain.services.product.ProductUpdateService;
 import com.liquorsgolden.lq.infrastructure.repository.order.OrderDtoMapper;
 import com.liquorsgolden.lq.infrastructure.repository.order.OrderRepository;
-import com.liquorsgolden.lq.shared.exception.message.product.ProductNotFoundException;
 import lombok.AllArgsConstructor;
-import com.liquorsgolden.lq.domain.entities.Order;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDateTime;
 
 @AllArgsConstructor
 @Service
