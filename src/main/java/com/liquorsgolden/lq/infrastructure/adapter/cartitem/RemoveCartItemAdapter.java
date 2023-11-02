e4038533526aadc4ed090a11261f2033ac0e4f2d
Ismael Trocha <ismael.trocha@hotmail.com>
2023-11-01 19:19:39 -0500

    Some changes

--- src/main/java/com/liquorsgolden/lq/infrastructure/adapter/cartitem/RemoveCartItemAdapter.java
+++ src/main/java/com/liquorsgolden/lq/infrastructure/adapter/cartitem/RemoveCartItemAdapter.java
@@ -1,19 +1,18 @@
 package com.liquorsgolden.lq.infrastructure.adapter.cartitem;
 
-import com.liquorsgolden.lq.domain.services.shoppingcart.RemoveItemShoppingCartService;
-import com.liquorsgolden.lq.infrastructure.repository.product.ProductRepository;
-import lombok.AllArgsConstructor;
+import com.liquorsgolden.lq.domain.services.cartitem.RemoveCartItemService;
+ import com.liquorsgolden.lq.infrastructure.repository.cartitem.CartItemRepository;
+ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
-public class RemoveCartItemAdapter implements RemoveItemShoppingCartService {
+public class RemoveCartItemAdapter implements RemoveCartItemService {
 
-  private final ProductRepository productRepository;
-
+  private final CartItemRepository cartItemRepository;
 
   @Override
-  public void removeItem(Long id) {
-    productRepository.deleteById(id);
+  public void removeCartItem(Long id) {
+    cartItemRepository.deleteById(id);
   }
 }
