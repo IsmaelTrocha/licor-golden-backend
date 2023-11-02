19cdb7e86079b3d96143808fc3dd35f8cfecbf18
Quintero45 <millerquintero1@gmail.com>
2023-11-01 21:50:12 -0500

    rebase

--- src/main/java/com/liquorsgolden/lq/infrastructure/repository/shoppingcart/cart/CartDto.java
+++ src/main/java/com/liquorsgolden/lq/infrastructure/repository/shoppingcart/cart/CartDto.java
@@ -1,22 +1,14 @@
 package com.liquorsgolden.lq.infrastructure.repository.shoppingcart.cart;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
-import com.liquorsgolden.lq.infrastructure.repository.product.ProductDto;
-import com.liquorsgolden.lq.infrastructure.repository.shoppingcart.cartitem.CartItemDto;
+import com.liquorsgolden.lq.infrastructure.repository.cartitem.CartItemDto;
 import com.liquorsgolden.lq.infrastructure.repository.user.CustomerDto;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
-import java.util.List;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @AllArgsConstructor
